f56238b2dbea23d10eb12c3b539fecaa81f7a0d2
github naoki4287 <github@waka.naoki86@icloud.com>
2022-04-08 15:50:38 +0900

    🩹 Blade Componentに書き換えたり、ルーティングの変更をしたりしました

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,10 @@
-@props(['disabled' => false])
+<input class="w-80 h-8 rounded-lg  border-2 border-blue-500 placeholder:pl-2 outline-none" type="text" name="title" placeholder="タイトル"><br>
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+
+
+
+
+
+<!-- @props(['disabled' => false])
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200  focus:ring-opacity-50']) !!}> -->
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,12 +1,9 @@
-@extends('layouts.app')
-
-@section('title', 'Create')
-
-@section('content')
-<div>
-  <form class="text-center mt-60" action="{{ route('post') }}" name="create_question" method="post">
-  @csrf
-    <input class="w-80 h-8 rounded-lg  border-2 border-blue-500 placeholder:pl-2 outline-none" type="text" name="title" placeholder="タイトル"><br>
-    <button class="mt-16 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">完了</button>
-  </form>
-</div>
+<x-app-layout>
+  <div>
+    <form class="text-center mt-60" action="{{ route('post') }}" name="create" method="post">
+      @csrf
+      <x-input></x-input>
+      <x-button>完了</x-button>
+    </form>
+  </div>
+</x-app-layout>
