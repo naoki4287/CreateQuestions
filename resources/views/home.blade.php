62593e044fed9239c453539db0dc49c32c7ffaf1
github naoki4287 <github@waka.naoki86@icloud.com>
2022-04-09 21:20:52 +0900

    ✨ とりあえずtitleをHomeページに表示できました。レイアウトは後日やります。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,15 @@
 <x-app-layout>
   <div>
-    @if (!isset($data))
-    <h1 class="text-center mt-80 text-3xl text-blue-500">{!! nl2br(e($msg)) !!}</h1>
-    @else
-    <ul>
-      @foreach ($data as $title) {
-      <li>{{ $title }}</li>
-      }
+    <!-- @if (!isset($titles))
+    <h1 class="text-center mt-80 text-3xl text-blue-500">作成したページはありません。<br>右下の＋ボタンを押して問題を作成してください。</h1>
+  
+    @else -->
+    <ul class="text-center mt-80 text-blue-500">
+      @foreach ($titles as $title) 
+        <li>{{ $title['title'] }}</li>
       @endforeach
     </ul>
-
-    @endif
+    <!-- @endif -->
 
     <form class="flex justify-end" action="{{ route('create') }}">
       <button class="mr-28 mt-60"><i class="fas fa-4x fa-plus-circle"></i></button>
