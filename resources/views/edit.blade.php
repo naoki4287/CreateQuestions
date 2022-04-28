845c4841fd583fe7f5f3be594ccc267fe22b9cd6
github naoki4287 <github@waka.naoki86@icloud.com>
2022-04-28 09:01:46 +0900

    ✨ autofocusをつけました

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
   <div>
-    <div class="text-white text-center ">
+    <div class="text-white text-center text-3xl relative top-32">
       {{ $edit_title['title']}}
     </div>
     <form class="text-center mt-60" action="{{ route('update') }}
@@ -19,7 +19,7 @@
       @error('answer')
       <div class="text-white text-center relative bottom-4">ERROR: {{ $message }}</div>
       @enderror
-      <textarea class="w-80 rounded-lg  placeholder:pl-2 placeholder:pt-1 outline-none resize-none" name="question" rows="3" placeholder="問題を作成してください" value="">{{ old('question') }}</textarea><br>
+      <textarea class="w-80 rounded-lg  placeholder:pl-2 placeholder:pt-1 outline-none resize-none" name="question" rows="3" placeholder="問題を作成してください" autofocus>{{ old('question') }}</textarea><br>
       <input class="w-80 h-8 rounded-lg placeholder:pl-2 outline-none" type="text" name="answer" placeholder="解答" value="{{ old('answer') }}">
     </form>
   </div>
