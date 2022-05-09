ed9ef3c0a09f99d6304adeeddcbbfc1f41dbef87
github naoki4287 <github@waka.naoki86@icloud.com>
2022-05-09 21:34:48 +0900

    ✨ ゲストログイン機能を実装しました

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $users = [
+      'name' => 'ゲスト',
+      'email' => 'guest@user',
+      'password' => 'password123'
+    ];
+    DB::table('users')->insert($users);
+  }
+}
