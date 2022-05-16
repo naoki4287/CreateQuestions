c11d911d87e9367b02f64437b6d43a5470676ee9
github naoki4287 <github@waka.naoki86@icloud.com>
2022-05-16 18:21:34 +0900

    ✨ テスト用DBを作成してUsersテーブルのテストを行いました。

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,40 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+  // use RefreshDatabase;
+  /**
+   * A basic test example.
+   *
+   * @return void
+   */
+  public function test_the_application_returns_a_successful_response()
+  {
+    // $response = $this->get('/');
+    $this->get('/')->assertStatus(302);
+    // $response->assertStatus(302);
 
-        $response->assertStatus(200);
-    }
+    // // $response->assertStatus(200); // 302にしてみる
+    // $this->get('/hoge')->assertStatus(404);
+    // $this->get('/create')->assertOk();
+    // $this->get('/questions/{id}')->assertOk();
+    // $this->get('/questionlist/{id}')->assertOk();
+    // $this->get('/add/{id}')->assertOk();
+    // // $this->post('/delete')->assertOk();
+
+
+    // $users = [
+    //   'id' => 2,
+    //   'name' => 'wakabashi',
+    //   'email' => 'waka@bayashi',
+    // ];
+    // $this->assertDatabaseHas('users', $users);
+
+    
+  }
 }
